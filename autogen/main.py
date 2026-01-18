50c9782e64ca322105a92cc8892a2f1e1d565b57
Thao Nguyen The <threezinedine@gmail.com>
2026-01-18 13:21:29 +0700

    [config] update configure for autogen with windows

--- autogen/main.py
+++ autogen/main.py
@@ -1,8 +1,11 @@
+import os
 from argparse import ArgumentParser
 from utils import *
 import logging
 from log import autogen_logger
 from autogen import Autogen
+from constants import *
+from clang.cindex import Config
 
 
 def main():
@@ -20,6 +23,12 @@ def main():
         autogen_logger.debug("Verbose mode enabled.")
     else:
         autogen_logger.setLevel(logging.INFO)
+
+    if os.name == "nt":
+        libclang_path = os.environ.get("NTT_LIBCLANG_PATH", "")
+        assert libclang_path, "Please set NTT_LIBCLANG_PATH environment variable."
+        autogen_logger.info("Setting libclang path for Windows.")
+        Config.set_library_path(libclang_path)
 
     autogen = Autogen()
     autogen.Execute()
